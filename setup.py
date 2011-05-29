7cd98bcee72803480a906155ffece122a28076ef
Roché Compaan <roche@upfrontsystems.co.za>
2011-05-29 12:59:10 +0200

    remove setup_requires and paster_plugins params

--- setup.py
+++ setup.py
@@ -32,6 +32,4 @@ setup(name='upfrontaccounting.theme',
       [z3c.autoinclude.plugin]
       target = plone
       """,
-      setup_requires=["PasteScript"],
-      paster_plugins = ["ZopeSkel"],
       )
